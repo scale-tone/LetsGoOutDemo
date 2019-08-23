f539c411c4d5729a4d75e3adcaa6a55ebd29494d
Konstantin Lepeshenkov <kolepes@microsoft.com>
2019-08-23 23:51:12 +0200

    Need to re-commit the Functions backend

---
+++ backend/LetsGoOutDemo.Functions/DataModel.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+
+namespace LetsGoOutDemo.Functions
+{
+    // Enum representing the appointment status
+    public enum AppointmentStatusEnum
+    {
+        Pending = 0,
+        Accepted,
+        Declined
+    }
+
+    // Internal representation of an appointment
+    public class Appointment
+    {
+        // The original list of participants
+        public HashSet<string> Participants { get; set; }
+        // The list of participants that have accepted the appointment
+        public HashSet<string> ParticipantsAccepted { get; set; }
+        // The current status of the appointment
+        public AppointmentStatusEnum Status { get; set; }
+
+        // Is the appointment accepted by all participants?
+        public bool HasEveryoneAccepted()
+        {
+            return this.Participants.SetEquals(this.ParticipantsAccepted);
+        }
+    }
+
+    // Internal representation of participant's response
+    public class AppointmentResponse
+    {
+        public string NickName{ get; set; }
+        public bool IsAccepted { get; set; }
+    }
+}
