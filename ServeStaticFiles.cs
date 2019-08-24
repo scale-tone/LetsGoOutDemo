6e98f27d472c2d476b1faabc47c79c9b52159d31
Konstantin Lepeshenkov <kolepes@microsoft.com>
2019-08-24 22:17:02 +0200

    minor improvements to statics hosting

--- ServeStaticFiles.cs
+++ ServeStaticFiles.cs
@@ -12,29 +12,30 @@ namespace LetsGoOutDemo.Functions
         // Serves static files for client UI
         [FunctionName("ui")]
         public static IActionResult Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "ui/{p1?}/{p2?}/{p3?}")] HttpRequest req
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "ui/{p1?}/{p2?}/{p3?}")] HttpRequest req,
+            ExecutionContext context
         )
         {
-            var path = req.Path.Value.Replace("/api/ui", wwwroot);
+            string root = context.FunctionAppDirectory;
+            string path = req.Path.Value;
             var contentType = FileMap.FirstOrDefault((kv => path.StartsWith(kv[0])));
             if(contentType != null)
             {
-                return File.Exists(path) ? 
-                    (IActionResult)new FileStreamResult(File.OpenRead(path), contentType[1]) : 
+                return File.Exists(root + path) ? 
+                    (IActionResult)new FileStreamResult(File.OpenRead(root + path), contentType[1]) : 
                     new NotFoundResult();
             }
             // Returning index.html by default, to support client routing
-            return new FileStreamResult(File.OpenRead($"{wwwroot}/index.html"), "text/html; charset=UTF-8");
+            return new FileStreamResult(File.OpenRead($"{root}/api/ui/index.html"), "text/html; charset=UTF-8");
         }
 
-        private const string wwwroot = "./wwwroot";
         private static readonly string[][] FileMap = new string[][]
         {
-            new [] {$"{wwwroot}/static/css/", "text/css; charset=utf-8"},
-            new [] {$"{wwwroot}/static/media/", "image/svg+xml; charset=UTF-8"},
-            new [] {$"{wwwroot}/static/js/", "application/javascript; charset=UTF-8"},
-            new [] {$"{wwwroot}/manifest.json", "application/json; charset=UTF-8"},
-            new [] {$"{wwwroot}/favicon.ico", "image/x-icon"}
+            new [] {"/api/ui/static/css/", "text/css; charset=utf-8"},
+            new [] {"/api/ui/static/media/", "image/svg+xml; charset=UTF-8"},
+            new [] {"/api/ui/static/js/", "application/javascript; charset=UTF-8"},
+            new [] {"/api/ui/manifest.json", "application/json; charset=UTF-8"},
+            new [] {"/api/ui/favicon.ico", "image/x-icon"}
         };
     }
 }
